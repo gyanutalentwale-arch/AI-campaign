d707566b2fd689b3d27aac3ab2031c3c89fb14ff
gyanutalentwale-arch <gyanu.talentwale@gmail.com>
2026-04-23 21:32:38 +0530

    feat: add temporary scripts and configuration for Azure web app creation

---
+++ .tmp-create-webapp.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" webapp create -g rg-wpbot-prod -p plan-wpbot-gyanu-202604232104419 -n wpbot-node-20260423211805 --runtime NODE^^|20-lts -o json
